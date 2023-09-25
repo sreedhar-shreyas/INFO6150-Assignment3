fa1da2e36841deefffc9048946779355035e7862
Sreedhar Shreyas <sreedhar.sh@northeastern.edu>
2023-09-24 23:25:34 -0400

    added more functionalities

--- script.js
+++ script.js
@@ -26,12 +26,13 @@ function addrecord() {
   
 
   // var studentName = table.lastElementChild.lastElementChild?.firstElementChild?.nextElementSibling?.innerHTML || "Student 0"
-  var studentName = table.lastElementChild.lastElementChild?.previousElementSibling?.firstElementChild?.nextElementSibling.innerHTML || "doesnt work"
+  var studentName = table.lastElementChild.lastElementChild?.previousElementSibling?.firstElementChild?.nextElementSibling.innerHTML || "Student 0"
   console.log(studentName); 
+  
   var lastIndex = studentName.split(" ")[1]; 
    var tableRow = document.createElement("tr");
    var tdCheckBoxNode = document.createElement("td");
-   tdCheckBoxNode.innerHTML = '<input type="checkbox" onclick="onClickCheckBox(this)" />'  
+   tdCheckBoxNode.innerHTML = '<input type="checkbox" onclick="onClickCheckBox(this)" /><br/><br/><img src="./down.png" width="25px" onclick="tableView()">'  
    var tableDataStudent = document.createElement("td");
    tableDataStudent.innerHTML = 'Student '+(parseInt(lastIndex)+1)
    var tableDataTeacher = document.createElement("td");
@@ -46,9 +47,10 @@ function addrecord() {
   tableDataBudget.innerHTML = '12344'
   var tableDataPercentage = document.createElement("td");
   tableDataPercentage.innerHTML = '100%'
-  var tableDataDelete = document.createElement("td");
-  tableDataDelete.innerHTML = '<button>Delete</button>'
+  // var tableDataDelete = document.createElement("td");
+  // tableDataDelete.innerHTML = '<button>Delete</button>'
   var trNode = document.createElement("tr");
+  trNode.className = "dropDownTextArea";  //broaddclasstoelement
   var tdAuthor9 = document.createElement("td");
   tdAuthor9.innerHTML = 'Advisor:<br /><br />Award Details<br />Summer 1-2014(TA)<br />Budget Number: <br />Tuition Number: <br />Comments:<br /><br /><br />Award Status:<br /><br /><br />'
 
@@ -69,7 +71,7 @@ function addrecord() {
 
    tableRow.appendChild(tableDataPercentage);
 
-   tableRow.appendChild(tableDataDelete);
+  //  tableRow.appendChild(tableDataDelete);
    tbody.appendChild(tableRow);
    
    trNode.appendChild(tdAuthor9);
@@ -77,9 +79,15 @@ function addrecord() {
 
    tbody.appendChild(trNode);
   
-  
-
-  
+  // alert(tableDataStudent.innerHTML+" Added Succesfully");
+// checkData();
+// var tblds=addrecord.studentName;
+// tbl3 = addrecord.tableDataStudent;
+if (studentName === null) {
+  alert( "Failed to add row");
+} else {
+    alert(tableDataStudent.innerHTML+" Added Succesfully");
+}
 
 }
 
@@ -92,3 +100,48 @@ function addrecord() {
 
 //   tbody.appendChild(tableRow1);
 // }
+
+function checkData() {
+ 
+}
+function tableView() {
+  var tbl = document.getElementsByClassName("dropDownTextArea");
+  console.log("first");
+  Array.from(tbl).forEach((x) => {
+    if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    console.log("broosecond");
+    x.style.display = "block";
+  }
+})
+};
+// function tableView() {
+// tabletoggle = document .querySelector (".dropDownTextArea");
+
+//   tabletoggle.classList.toggle("hide");
+// }
+function onClickCheckBox(checkBox){
+  console.log(checkBox);
+  var table = document.getElementById("myTable");
+  
+  var adddelcol = table.firstElementChild.firstElementChild.lastElementChild;
+  var selcheckboxrow = checkBox.parentElement.parentElement;
+  if(checkBox.checked == true)
+  {
+    selcheckboxrow.style.backgroundColor="Yellow";
+    var deleteButtonHeader = document.createElement("th");
+    deleteButtonHeader.innerHTML = 'Delete'
+    adddelcol.appendChild(deleteButtonHeader);
+    var deleteButton = document.createElement("td");
+    deleteButton.innerHTML = '<button id="deleted" type="button" onClick="onDelete(this)" >Delete</button>'
+    
+    selcheckboxrow.appendChild(deleteButton);
+  }
+  else{
+    selcheckboxrow.style.backgroundColor="White";
+    selcheckboxrow.deleteCell(8)
+    // selcheckboxrow.deleteColumn(8)
+  }
+
+}
